6a466dc490c37a27c6f42fbbf38e5fa1eb80fe1c
manio4444 <kalinowskixmarcin@o2.pl>
2020-07-14 02:18:28 +0200

    mmenu 6.1.8 ready

--- src/js/header/menu-mobile.js
+++ src/js/header/menu-mobile.js
@@ -2,12 +2,25 @@ import $ from 'jquery';
 window.$ = $;
 window.jQuery = $;
 
+import 'jquery.mmenu';
+
 $(document).ready(function() {
   const $hamburger = $('.header-navigation__hamburger');
-  const $mobile_overlay = $('.header-navigation__menu');
+  const $mobileContent = $('.header-navigation__menu-mobile');
+  let mmenu = $($mobileContent).mmenu();
 
   $hamburger.click(function () {
-    $mobile_overlay.toggleClass('is-active');
+    if ($hamburger.hasClass('close')) {
+      mmenu.data("mmenu").close();
+      $hamburger.removeClass('close');
+    } else {
+      mmenu.data("mmenu").open();
+      $hamburger.addClass('close');
+    }
   });
 
+  $('#mm-blocker').click(function () {
+    mmenu.data("mmenu").close();
+    $hamburger.removeClass('close');
+  });
 });
